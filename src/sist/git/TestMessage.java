bbf407585fefab01cda6200a7eecb2f4b9b14131
sss2586 <wnsghrnt2@naver.com>
2018-10-05 15:14:32 +0900

    FirstMOD2

--- src/sist/git/TestMessage.java
+++ src/sist/git/TestMessage.java
@@ -5,6 +5,8 @@ public class TestMessage {
 	public static void main(String[] args) {
 		System.out.println("HellowMessage");
 		System.out.println("아나까나");
+		System.out.println("추가");
+		System.out.println("AAA");
 	}
 
 }
